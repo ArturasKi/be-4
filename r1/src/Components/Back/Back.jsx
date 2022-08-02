d075ccbb62d48b2afac7ac6b32e8001c06bad419
Arturito <arturaskisielius91@gmail.com>
2022-08-02 12:02:59 +0300

    Update: back, front components

---
+++ r1/src/Components/Back/Back.jsx
@@ -0,0 +1,7 @@
+function Back() {
+    return (
+        <h1>Back</h1>
+    )
+}
+
+export default Back;
---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+const cors = require("cors");
+app.use(express.json({ limit: '10mb' }));
+app.use(cors());
+const mysql = require("mysql");
+const md5 = require('js-md5');
+const uuid = require('uuid');
+
+app.use(
+  express.urlencoded({
+      extended: true,
+  })
+);
+app.use(express.json());
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "viesasis_sektorius",
+});
